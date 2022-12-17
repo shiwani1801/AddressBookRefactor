8a07b48b09fccf756d983fa79cb229fba28a9c78
shiwani <shiwanipaunikar1@gmail.com>
2022-12-17 14:54:32 -0800

    [shiwani]address book code added

---
+++ src/com/bridgelabz/addressbook/RefactorAddressBook.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.addressbook;
+
+public class RefactorAddressBook {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address book program");
+    }
+
+}
